6e21502060bcbb35851c0920f2c468a140355c37
rafaelmendes1 <rarafafa3245@gmail.com>
2021-08-23 11:40:20 -0300

    Room entity developed

---
+++ src/main/java/com/github/rafaelmendes1/meetingapi/entities/Room.java
@@ -0,0 +1,86 @@
+package com.github.rafaelmendes1.meetingapi.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "meetingroom")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String date;
+
+    @Column(nullable = false)
+    private String startHour;
+
+    @Column(nullable = false)
+    private String endHour;
+
+    public Room(){
+    }
+
+    public Room(Long id, String name, String date, String startHour, String endHour) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.startHour = startHour;
+        this.endHour = endHour;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getStartHour() {
+        return startHour;
+    }
+
+    public void setStartHour(String startHour) {
+        this.startHour = startHour;
+    }
+
+    public String getEndHour() {
+        return endHour;
+    }
+
+    public void setEndHour(String endHour) {
+        this.endHour = endHour;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", date='" + date + '\'' +
+                ", startHour='" + startHour + '\'' +
+                ", endHour='" + endHour + '\'' +
+                '}';
+    }
+}
