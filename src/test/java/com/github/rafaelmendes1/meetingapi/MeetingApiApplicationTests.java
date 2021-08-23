5f9860e62f460b814a8f85c14931fdbfec4b35cc
rafaelmendes1 <rarafafa3245@gmail.com>
2021-08-23 11:39:33 -0300

    Project created

---
+++ src/test/java/com/github/rafaelmendes1/meetingapi/MeetingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rafaelmendes1.meetingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
